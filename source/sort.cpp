f4defda4daa5c5e1232724a0391ca55dbaf2d8ec
Brandon Clearwater <brandon@clearwaterconceptions.com>
2022-08-18 17:02:10 -0600

    Added line counter progress output

--- source/sort.cpp
+++ source/sort.cpp
@@ -3,15 +3,22 @@
 #include <string>
 #include "../include/sort.hpp"
 
-bool alphabetize(std::ifstream& test, std::ofstream& output)
+bool alphabetize(std::ifstream &test, std::ofstream &output)
 {
+    int counter = 0;
     std::string line;
-    while(!test.eof())
+    while (!test.eof())
     {
+        ++counter;
         getline(test, line);
         output << line << std::endl;
+        if (counter % 10000 == 0)
+        {
+            std::cout << counter << " lines completed" << std::endl;
+        }
     }
-    //int compare (const string& string-name) const;
+    std::cout << "Completed, " << counter << " total lines" << std::endl;
+    // int compare (const string& string-name) const;
     return true;
 }
 
